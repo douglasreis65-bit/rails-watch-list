7b54613ec8fe6020aeb2781ceb5fc757cb579e25
Douglas Reis <douglas.reis65@gmail.com>
2026-03-10 16:22:57 -0300

    add comment to lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_09_143347) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_10_192154) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_09_143347) do
   end
 
   create_table "lists", force: :cascade do |t|
+    t.text "comment"
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
